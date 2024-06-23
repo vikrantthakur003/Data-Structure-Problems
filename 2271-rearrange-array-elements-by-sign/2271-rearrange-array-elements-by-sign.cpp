6f108f6ff3acbbda673618bc53a06ddacd0da921
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2024-06-23 22:37:21 +0530

    Time: 141 ms (64.66%), Space: 126.3 MB (66.45%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
-    vector<int> findNegativeNumber(vector<int>&arr,int n){
-        vector<int>negativeArray;
-        for(int i=0;i<n;i++){
-            if(arr[i] < 0){
-                negativeArray.push_back(arr[i]);
-            }
-        }
-        return negativeArray;
-    }
-    vector<int> findPositiveNumber(vector<int>&arr,int n){
-        vector<int>positiveArray;
-        for(int i=0;i<n;i++){
-            if(arr[i] > 0){
-                positiveArray.push_back(arr[i]);
-            }
-        }
-        return positiveArray;
-    }
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>negativeArray = findNegativeNumber(nums,nums.size());
-        vector<int>positiveArray = findPositiveNumber(nums,nums.size());
-        int k = 0;
-        int i=0;
-        int j=0;
-        while(i< negativeArray.size() || j<positiveArray.size()){
-            if(k%2 != 0){
-                nums[k] = negativeArray[i];
-                i++;
+        vector<int>ans(nums.size());
+        int oddIndex = 1;
+        int evenIndex = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] > 0){
+                ans[evenIndex] = nums[i];
+                evenIndex += 2;
             }else{
-                nums[k] = positiveArray[j];
-                j++;
+                ans[oddIndex] = nums[i];
+                oddIndex += 2;
             }
-            k++;
         }
-        return nums;
+        return ans;
     }
 };
