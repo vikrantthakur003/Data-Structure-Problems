3f5714fa2ebf3cae4818ee6dcc27193fccc75b55
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2024-06-24 10:45:51 +0530

    Time: 0 ms (100%), Space: 14.5 MB (54.27%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int minIndex = -1;
+        for(int i=nums.size()-2;i>=0;i--){
+            if(nums[i] < nums[i+1]){
+                minIndex = i;
+                break;
+            }
+        }
+        if(minIndex == -1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+
+        for(int i=nums.size()-1;i>minIndex;i--){
+            if(nums[i] > nums[minIndex]){
+                swap(nums[i],nums[minIndex]);
+                break;
+            }
+        }
+        reverse(nums.begin()+minIndex+1, nums.end());
     }
 };
