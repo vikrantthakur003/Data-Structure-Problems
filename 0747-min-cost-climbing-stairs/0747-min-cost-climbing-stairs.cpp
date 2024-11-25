96269a7ee4b5e3d734f26358564774acb8e87395
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2024-11-25 23:57:24 +0530

    Time: 0 ms (100%), Space: 16.6 MB (73.74%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     int recursionAppraoch(int n, vector<int>&cost, vector<int>&dp){
-        if(n == 0){
-            return cost[0]; 
-        }
-        if(n == 1){
-            return cost[1];
-        }
+        if(n == 0) return cost[0]; 
+        if(n == 1) return cost[1];
 
         if(dp[n] != -1){
             return dp[n];
         }
-
         return dp[n] = min(recursionAppraoch(n-1,cost,dp), recursionAppraoch(n-2, cost,dp)) + cost[n];
+    }
+
+    int tabulationAppraoch(int n, vector<int>&cost){
+        vector<int>dp(n+1, -1);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i=2;i<n;i++){
+            dp[i] = min(dp[i-1],dp[i-2]) + cost[i];
+        }
+        return min(dp[n-1], dp[n-2]);
     }
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int>dp(n+1, -1);
-        return min(recursionAppraoch(n-1, cost, dp), recursionAppraoch(n-2, cost,dp));
+        // vector<int>dp(n+1, -1);
+        // return min(recursionAppraoch(n-1, cost, dp), recursionAppraoch(n-2, cost,dp));
+
+        return tabulationAppraoch(n, cost);
     }
 };
