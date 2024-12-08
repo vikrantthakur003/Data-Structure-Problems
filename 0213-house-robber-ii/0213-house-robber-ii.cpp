bb4577e93efc2bceebe006d3862feb63ffbefd95
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2024-12-08 13:58:05 +0530

    Time: 0 ms (100%), Space: 11 MB (5.72%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -25,34 +25,35 @@ public:
         return dp[start] = max(included, excluded);
     }
     int tabulation(int start, int end, vector<int>&nums){
-        
-        vector<int>dp(end, 0);
+        int n = end-start+1;
+        vector<int>dp(n, 0);
         dp[0] = nums[start];
+        if (n > 1) dp[1] = max(nums[start], nums[start + 1]);
 
-        for(int i=start;i<=end; i++){
-            int included = dp[i+2] + nums[i];
-            int excluded = dp[i+1];
+        for(int i=2;i<n; i++){
+            int included = dp[i-2] + nums[start+i];
+            int excluded = dp[i-1];
 
             dp[i] = max(included, excluded);
         }
-        return dp[end];
+        return dp[n-1];
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
 
         if(n == 1) return nums[0];
         if(n==2) return max(nums[0],nums[1]);
-        vector<int>dp1(n, -1);
-        vector<int>dp2(n, -1);
+        // vector<int>dp1(n, -1);
+        // vector<int>dp2(n, -1);
 
         // int firstValueTake = recursion(0, n-2, nums);
         // int lastValueTaks = recursion(1, n-1, nums);
 
-        int firstValueTake = memo(0, n-2, nums, dp1);
-        int lastValueTaks = memo(1, n-1, nums, dp2);
+        // int firstValueTake = memo(0, n-2, nums, dp1);
+        // int lastValueTaks = memo(1, n-1, nums, dp2);
 
-        // int firstValueTake = tabulation(0, n-2, nums);
-        // int lastValueTaks = tabulation(1, n-1, nums);
+        int firstValueTake = tabulation(0, n-2, nums);
+        int lastValueTaks = tabulation(1, n-1, nums);
         return max(firstValueTake, lastValueTaks);
     }
 };
