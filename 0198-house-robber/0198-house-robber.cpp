eb99d5b9e53092bc48f4c9f9d57e9de20a61ccdd
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2024-12-08 11:39:28 +0530

    Time: 0 ms (100%), Space: 10.9 MB (5.55%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    // int solveUsingRecursion(vector<int>&nums, int n){
-    //     if(n == 0){
-    //         return nums[0];
-    //     }
-    //     if(n < 0){
-    //         return 0;
-    //     }
-
-    //     int inclaudeCurrentElement = solveUsingRecursion(nums, n-2) + nums[n];
-    //     int exclaudeCurrentElement = solveUsingRecursion(nums, n-1) + 0;
-
-    //     return max(inclaudeCurrentElement,exclaudeCurrentElement);
-    // }
-
-    int solveUsingMemo(vector<int>&nums, int n, vector<int>&dp){
-        if(n == 0){
+    int recursion(int n,vector<int>&nums){
+        if(n==0){
             return nums[0];
         }
-        if(n < 0){
+        if(n<0){
             return 0;
         }
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        int inclaudeCurrentElement = solveUsingMemo(nums, n-2,dp) + nums[n];
-        int exclaudeCurrentElement = solveUsingMemo(nums, n-1,dp) + 0;
+        int included = recursion(n-2, nums) + nums[n];
+        int excluded = recursion(n-1,nums);
 
-        return dp[n] = max(inclaudeCurrentElement,exclaudeCurrentElement);
+        return max(included, excluded);
+    }
+
+    int tabulation(vector<int>&nums){
+        int n = nums.size();
+        vector<int>dp(n, 0);
+        if(n==1){
+            return nums[0];
+        }
+        dp[0] = nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            int included = dp[i-2] + nums[i];
+            int excluded = dp[i-1];
+
+            dp[i] = max(included, excluded);
+        }
+
+        return dp[n-1];
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        // return solveUsingRecursion(nums, n-1);
-        vector<int>dp(n, -1);
-        return solveUsingMemo(nums, n-1, dp);
+        // return recursion(n-1, nums);
+        return tabulation(nums);
     }
 };
