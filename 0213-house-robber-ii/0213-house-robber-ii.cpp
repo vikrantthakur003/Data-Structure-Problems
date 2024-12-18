c30caba222d4b2a066bb76cd8080ef0f60d28b1d
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2024-12-18 22:47:26 +0530

    Time: 0 ms (100%), Space: 11 MB (8.91%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,59 +1,37 @@
 class Solution {
 public:
-    int recursion(int start, int end, vector<int>&nums){
-        if(start > end){
-            return 0;
-        }
-        int included = recursion(start+2, end, nums) + nums[start];
-        int excluded = recursion(start+1,end, nums);
-
-        return max(included, excluded);
-    }
-
-    int memo(int start, int end, vector<int>&nums, vector<int>&dp){
-        if(start > end){
-            return 0;
-        }
-
-        if(dp[start] != -1){
-            return dp[start];
-        }
-
-        int included = memo(start+2, end, nums,dp) + nums[start];
-        int excluded = memo(start+1, end, nums,dp);
-
-        return dp[start] = max(included, excluded);
-    }
-    int tabulation(int start, int end, vector<int>&nums){
-        int n = end-start+1;
+    int helperRobber(vector<int>&nums, int n){
         vector<int>dp(n, 0);
-        dp[0] = nums[start];
-        if (n > 1) dp[1] = max(nums[start], nums[start + 1]);
+        if(n < 2){
+            return nums[0];
+        }
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
 
-        for(int i=2;i<n; i++){
-            int included = dp[i-2] + nums[start+i];
-            int excluded = dp[i-1];
+        for(int i=2;i<n;i++){
+            int includeElement = dp[i-2] + nums[i];
+            int excludeElement = dp[i-1];
 
-            dp[i] = max(included, excluded);
+            dp[i] = max(includeElement, excludeElement);
         }
         return dp[n-1];
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
+        if(n < 2)
+            return nums[0];
 
-        if(n == 1) return nums[0];
-        if(n==2) return max(nums[0],nums[1]);
-        // vector<int>dp1(n, -1);
-        // vector<int>dp2(n, -1);
+        vector<int>skipFirstElement(n-1);
+        vector<int>skipLastElement(n-1);
 
-        // int firstValueTake = recursion(0, n-2, nums);
-        // int lastValueTaks = recursion(1, n-1, nums);
+        for(int i=0;i<n-1;i++){
+            skipFirstElement[i] = nums[i+1];
+            skipLastElement[i] = nums[i];
+        }
 
-        // int firstValueTake = memo(0, n-2, nums, dp1);
-        // int lastValueTaks = memo(1, n-1, nums, dp2);
+        int firstSkippingMaximum = helperRobber(skipFirstElement, skipFirstElement.size());
+        int lastSkippingMaximum = helperRobber(skipLastElement, skipLastElement.size());
 
-        int firstValueTake = tabulation(0, n-2, nums);
-        int lastValueTaks = tabulation(1, n-1, nums);
-        return max(firstValueTake, lastValueTaks);
+        return max(firstSkippingMaximum,lastSkippingMaximum);
     }
 };
