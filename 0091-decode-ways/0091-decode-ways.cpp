79bcb5b48ea5952cc4511156566b6286ccec86ba
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2024-12-20 21:56:34 +0530

    Time: 0 ms (100%), Space: 9.7 MB (22.94%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,46 +1,28 @@
 class Solution {
 public:
-    int helperRecursion(const string& str, int index){
-        if(str[index] == '0'){
-            return 0;
-        }
+    int helperRecursion(string &str, int index, vector<int>&dp){
         if(index == str.size()){
             return 1;
         }
-
-        int oneDigitDecoded = helperRecursion(str, index+1);
-        int twoDigit = 0;
-        if(index + 1 < str.size()) {
-        int num = stoi(str.substr(index, 2));
-            if(num >= 10 && num <= 26 ){
-            twoDigit = helperRecursion(str, index+2);
-            }
+        if(str[index] == '0'){
+            return 0;
         }
-        
-        return oneDigitDecoded + twoDigit;
-    }
+        if(dp[index] != -1){
+            return dp[index];
+        }
 
-    int helperMemo(string &str, vector<int>&dp, int index){
-        if(str[index] == '0') return 0;
-        if(index == str.size()) return 1;
-
-        if(dp[index] != -1) return dp[index];
-
-        int oneDigitDecoded = helperMemo(str, dp, index+1);
+        int oneDigitDecoded = helperRecursion(str, index+1, dp);
         int twoDigitDecoded = 0;
-
         if(index + 1 < str.size()){
             int num = stoi(str.substr(index, 2));
-            if(num >=10 && num <=26 ){
-                twoDigitDecoded = helperMemo(str, dp, index+2);
+            if(num >= 10 && num <=26){
+                twoDigitDecoded = helperRecursion(str, index+2, dp);
             }
         }
-
         return dp[index] = oneDigitDecoded + twoDigitDecoded;
     }
     int numDecodings(string s) {
-        // return helperRecursion(s,0);
-        vector<int>dp(s.size() + 1, -1);
-        return helperMemo(s,dp, 0);
+        vector<int>dp(s.size()+1, -1);
+        return helperRecursion(s, 0, dp);
     }
 };
