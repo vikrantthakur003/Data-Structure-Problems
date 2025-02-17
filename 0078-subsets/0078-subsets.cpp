44a588a52c2de4b52f37b85b07cf030029e58d89
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-02-17 11:25:11 +0530

    Time: 3 ms (33.94%), Space: 15.7 MB (14.13%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    void findSubsets(vector<int>&nums, vector<vector<int>> &result, vector<int> &temp, int index){
+    vector<vector<int>> result;
+    void findSubsets(vector<int>& nums, int index, vector<int> temp){
         if(index >= nums.size()){
             result.push_back(temp);
             return;
         }
+
         temp.push_back(nums[index]);
-        findSubsets(nums, result,temp, index + 1);
+        findSubsets(nums, index + 1, temp);
 
         temp.pop_back();
-        findSubsets(nums, result, temp, index + 1);
 
+        findSubsets(nums, index + 1, temp);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
         vector<int> temp;
-        findSubsets(nums, result,temp, 0);
+        findSubsets(nums, 0, temp);
 
         return result;
     }
