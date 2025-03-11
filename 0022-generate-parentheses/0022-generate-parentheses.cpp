fbde28bef3fb357b03120b57e77a141eb016de53
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-03-11 21:31:36 +0530

    Time: 105 ms (5.06%), Space: 91.3 MB (5.02%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
-    void findParenthesis(vector<string> &result, string temp, int n, int open, int close){
-        if(temp.size() == 2*n){
-            result.push_back(temp);
+    bool validParenthesis(string str){
+       int count = 0;
+       for(int i=0;i<str.size();i++){
+            if(str[i] == '('){
+                count++;
+            } else{
+                count--;
+            }
+
+            if(count < 0){
+                return false;
+            }
+       }
+        return count == 0;
+    }
+    void generate(int n, int index, string temp, vector<string> &result){
+        if(index >= 2*n){
+            if(validParenthesis(temp)){
+                result.push_back(temp);
+            }
             return;
         }
-        if(open < n){
-            findParenthesis(result, temp + '(', n, open + 1, close);
-        }
-        if(close < open){
-            findParenthesis(result, temp + ')', n, open, close + 1);
-        }
+        temp.push_back('(');
+        generate(n, index + 1, temp, result);
+        temp.pop_back();
+
+        temp.push_back(')');
+        generate(n, index + 1, temp, result);
+        temp.pop_back();
     }
     vector<string> generateParenthesis(int n) {
         vector<string> result;
         string temp = "";
-        findParenthesis(result, temp, n, 0, 0);
+        generate(n, 0, temp, result);
 
         return result;
     }
