cf41405b20a1bd7ff2278eddaaddb6077abcd072
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-05-28 23:30:48 +0530

    Time: 3 ms (52.63%), Space: 38.2 MB (35.3%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -43,11 +43,65 @@ public:
 
         return false;
     }
-    bool findTarget(TreeNode* root, int k) {
-        vector<int> inorder;
-        inorderTraversal(root, inorder);
-        return isExistsTarget(inorder, k);
+    stack<TreeNode*> ascendingStack;
+    stack<TreeNode*> descendingStack;
+    void pushElements(TreeNode* node, string type){
+        while(node != nullptr){
+            if(type == "LEFT"){
+                ascendingStack.push(node);
+                node = node->left;
+            } else {
+                descendingStack.push(node);
+                node = node->right;
+            }
+        }
+    }
 
+    TreeNode* next(){
+        TreeNode* topNode = ascendingStack.top();
+        ascendingStack.pop();
+        pushElements(topNode->right, "LEFT");
+
+        return topNode;
+    }
+
+    TreeNode* before(){
+        TreeNode* topNode = descendingStack.top();
+        descendingStack.pop();
+        pushElements(topNode->left, "RIGHT");
+
+        return topNode;
+    }
+
+    bool isExistsSum(TreeNode* root, int target){
+
+        pushElements(root, "LEFT");
+        pushElements(root, "RIGHT");
+
+        TreeNode* start = next();
+        TreeNode* end = before();
+
+        while(start != end){
+            int sum = start->val + end->val;
+
+            if(sum == target){
+                return true;
+            } else if(sum > target){
+                end = before();
+            } else {
+                start = next();
+            }
+        }
+
+        return false;
+    }
+
+
+    bool findTarget(TreeNode* root, int k) {
+        // vector<int> inorder;
+        // inorderTraversal(root, inorder);
+        // return isExistsTarget(inorder, k);
         
+        return isExistsSum(root, k);
     }
 };
