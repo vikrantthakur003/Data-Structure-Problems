c8012de990325e4345c5241232f7e3918dab6b76
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-06-09 19:07:11 +0530

    Time: 21 ms (51.49%), Space: 34.9 MB (66.74%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,36 +4,39 @@ public:
         int n = mat.size();
         int m = mat[0].size();
 
+        vector<vector<int>> result(n, vector<int> (m, -1));
         queue<pair<int, int>> q;
-        vector<vector<int>> result(n, vector<int>(m, -1));
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 0) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j] == 0){
                     q.push({i, j});
                     result[i][j] = 0;
                 }
             }
         }
 
-          vector<pair<int, int>> directions = {
-                {-1, 0}, {1, 0}, {0, -1}, {0, 1}
-            };
+        vector<pair<int, int>> directions = {
+            {-1, 0},
+            {1, 0},
+            {0, -1},
+            {0, 1}
+        };
 
-        while (!q.empty()) {
+        while(!q.empty()){
             pair<int, int> frontPair = q.front();
             q.pop();
 
             int x = frontPair.first;
             int y = frontPair.second;
-        
-            for (auto dir : directions) {
+
+            for(auto dir : directions){
                 int newX = x + dir.first;
                 int newY = y + dir.second;
 
-                if (newX >= 0 && newX < n && newY >= 0 && newY < m &&
-                    result[newX][newY] == -1) {
+                if(newX >=0 && newX < n && newY >=0 && newY < m && result[newX][newY] == -1){
                     result[newX][newY] = result[x][y] + 1;
+
                     q.push({newX, newY});
                 }
             }
