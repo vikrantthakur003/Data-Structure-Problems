fa83abff6db85287ccc25019dff9af954608ae5e
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-06-09 16:59:36 +0530

    Time: 0 ms (100%), Space: 16.8 MB (60.67%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,51 +1,59 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int rottenMinutes = 0;
-        int totalRottanOrganes = 0;
-        int countRottonOrganes = 0;
-        queue<pair<int, int>> q;
+        int n = grid.size();
+        int m = grid[0].size();
 
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[i].size(); j++) {
-                if (grid[i][j] != 0) {
-                    totalRottanOrganes++;
+        int totalFreshOranges = 0;
+        int totalTimeToRotten = 0;
+        int countOfRottenOranges = 0;
+
+
+        queue<pair<int, int> > q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 1){
+                    totalFreshOranges++;
                 }
-                if (grid[i][j] == 2) {
+                if(grid[i][j] == 2){
                     q.push({i, j});
                 }
             }
         }
 
-        while (!q.empty()) {
-            int rotanOrganesSize = q.size();
-            countRottonOrganes += rotanOrganesSize;
+        vector<pair<int, int>> directions = {
+            {-1, 0},
+            {1, 0},
+            {0, -1},
+            {0, 1}
+        };
 
-            while (rotanOrganesSize--) {
+        while(!q.empty()){
+            int size = q.size();
+
+            while(size--){
                 pair<int, int> frontPair = q.front();
                 q.pop();
+
                 int x = frontPair.first;
                 int y = frontPair.second;
 
-                vector<pair<int, int>> directions = {
-                    {-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-                for (auto dir : directions) {
+                for(auto dir : directions){
                     int newX = x + dir.first;
                     int newY = y + dir.second;
 
-                    if (newX < grid.size() && newX >= 0 && newY >= 0 &&
-                        newY < grid[0].size() && grid[newX][newY] == 1) {
+                    if(newX >= 0 && newX < n && newY >= 0 && newY < m && grid[newX][newY] == 1){
+                        countOfRottenOranges++;
                         grid[newX][newY] = 2;
                         q.push({newX, newY});
                     }
                 }
             }
-
             if(!q.empty()){
-                rottenMinutes++;
+                totalTimeToRotten++;
             }
         }
-
-        return (countRottonOrganes == totalRottanOrganes) ? rottenMinutes : -1;
+        
+        return totalFreshOranges == countOfRottenOranges ? totalTimeToRotten : -1;
     }
 };
