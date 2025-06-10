9da83253eb4e540635505710a54339a3e340be4d
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-06-10 16:55:17 +0530

    Time: 0 ms (100%), Space: 14.8 MB (37.74%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
+        queue<pair<int,int>> q;
+
         int n = board.size();
         int m = board[0].size();
 
-        queue<pair<int, int>> q;
-
-        for(int j=0;j<m;j++){
-            // first row
-            if(board[0][j] == 'O'){
-                q.push({0, j});
-            } 
-            if(board[n-1][j] == 'O'){
-                q.push({n-1, j});
+      
+        for(int i=0;i<m;i++){
+              // first rows
+            if(board[0][i] == 'O'){
+                q.push({0, i});
             }
-        };
+            // last rows
+            if(board[n-1][i] =='O'){
+                q.push({n-1, i});
+            }
+        }
 
         for(int i=0;i<n;i++){
+            // first col
             if(board[i][0] == 'O'){
                 q.push({i, 0});
             }
 
+            // last col
             if(board[i][m-1] == 'O'){
                 q.push({i, m-1});
             }
-        };
+        }
 
         vector<pair<int, int>> directions = {
             {-1, 0},
@@ -34,11 +38,12 @@ public:
         };
 
         while(!q.empty()){
-            pair<int,int> frontPair = q.front();
+            pair<int, int> frontPair = q.front();
             q.pop();
 
             int x = frontPair.first;
             int y = frontPair.second;
+
             board[x][y] = 'S';
 
             for(auto dir : directions){
@@ -46,12 +51,11 @@ public:
                 int newY = y + dir.second;
 
                 if(newX >= 0 && newX < n && newY >= 0 && newY < m && board[newX][newY] == 'O'){
-                    board[newX][newY] = 'S';
                     q.push({newX, newY});
+                    board[newX][newY] = 'S';
                 }
             }
-        };
-
+        }
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
