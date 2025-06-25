758260762338990d026ba896b250cc5832827ae5
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-06-25 11:20:33 +0530

    Time: 8 ms (38.16%), Space: 22 MB (6.74%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    unordered_map<int, list<int>> adj;
     unordered_map<int, bool> visited;
-
-    bool dfsTraversal(int src, unordered_map<int, bool> &pathVisited){
+    unordered_map<int, bool> pathVisited;
+    bool cycleDetect(int src, unordered_map<int, list<int>> &adj){
         visited[src] = true;
         pathVisited[src] = true;
 
         for(auto neighbour : adj[src]){
             if(visited[neighbour] && pathVisited[neighbour]){
                 return true;
-            } else if(!visited[neighbour]){
-                bool isCyclePresent = dfsTraversal(neighbour, pathVisited);
+            } else if (!visited[neighbour]){
+                bool isCyclePresent = cycleDetect(neighbour, adj);
                 if(isCyclePresent){
                     return true;
                 }
-            }
+            } 
         }
 
         pathVisited[src] = false;
         return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, bool> pathVisited;
-        
+        unordered_map<int, list<int>> adj;
         for(int i=0;i<prerequisites.size();i++){
             int u = prerequisites[i][0];
             int v = prerequisites[i][1];
@@ -31,11 +29,11 @@ public:
             adj[u].push_back(v);
         }
 
-        for(int i=0;i< numCourses;i++){
+        for(int i=0;i<numCourses;i++){
             if(!visited[i]){
-               if(dfsTraversal(i, pathVisited)){
+                if(cycleDetect(i, adj)){
                     return false;
-               }
+                }
             }
         }
 
