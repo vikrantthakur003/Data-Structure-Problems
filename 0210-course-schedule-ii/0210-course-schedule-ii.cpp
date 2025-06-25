78cc694d593cd19f83b7bc7c964c32ed711c2267
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-06-25 12:07:31 +0530

    Time: 11 ms (12.17%), Space: 20.5 MB (5.19%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,35 +1,9 @@
 class Solution {
 public:
-    unordered_map<int, list<int>> adj;
     unordered_map<int, bool> visited;
-
+    unordered_map<int, bool> pathVisited;
     stack<int> st;
-    bool isCycleFound = false;
-    void dfs(int src, unordered_map<int, bool> &pathVisited){
-        visited[src] = true;
-        pathVisited[src] = true;
-
-        for(auto neighbour : adj[src]){
-            if(visited[neighbour] && pathVisited[neighbour]){
-                isCycleFound = true;
-                return;
-            }
-            if(!visited[neighbour]){
-                if(isCycleFound){
-                    return;
-                } else {  
-                    dfs(neighbour, pathVisited);
-                }
-               
-            }
-        }
-
-        st.push(src);
-        pathVisited[src] = false;
-        return;
-    }
-
-    bool findCycle(int src, unordered_map<int, bool> pathVisited){
+    bool detetCycle(int src, unordered_map<int, list<int>> &adj){
         visited[src] = true;
         pathVisited[src] = true;
 
@@ -37,19 +11,21 @@ public:
             if(visited[neighbour] && pathVisited[neighbour]){
                 return true;
             } else if(!visited[neighbour]){
-                bool isCycle = findCycle(neighbour, pathVisited);
-                if(isCycle){
+                bool isCyclePresent = detetCycle(neighbour, adj);
+                if(isCyclePresent){
                     return true;
                 }
             }
         }
+
         pathVisited[src] = false;
+        st.push(src);
 
         return false;
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, bool> pathVisited;
-        
+        unordered_map<int, list<int>> adj;
+
         for(int i=0;i<prerequisites.size();i++){
             int u = prerequisites[i][0];
             int v = prerequisites[i][1];
@@ -57,33 +33,22 @@ public:
             adj[u].push_back(v);
         }
 
-        // for(int i=0;i<numCourses;i++){
-        //     if(!visited[i]){
-        //         if(findCycle(i, pathVisited)){
-        //            return {};
-        //         }
-        //     }
-        // }
-
-        // visited.clear();
-
         for(int i=0;i<numCourses;i++){
             if(!visited[i]){
-                dfs(i, pathVisited);
-                if(isCycleFound){
+                if(detetCycle(i, adj)){
                     return {};
                 }
             }
         }
 
-        vector<int> result;
-
+        vector<int> ans;
         while(!st.empty()){
-            result.push_back(st.top());
+            ans.push_back(st.top());
             st.pop();
         }
 
-        reverse(result.begin(), result.end());
-        return result;
+        reverse(ans.begin(), ans.end());
+
+        return ans;
     }
 };
