77ef60285fd1ba451c2cf8e42aebae8617d653e4
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-06-28 23:03:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -21,11 +21,35 @@ public:
             return nums[0];
         }
         
-        int firstElementTake = houseRobber(0, nums, n - 2, dp);
-        for(int i=0;i<dp.size();i++){
-            dp[i] = -1;
+        // int firstElementTake = houseRobber(0, nums, n - 2, dp);
+        // fill(dp.begin(), dp.end(), -1);
+        // int lastElementTake = houseRobber(1, nums, n - 1, dp);
+
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for(int i=2;i<n-1;i++){
+            int takeValue = nums[i] + dp[i-2];
+            int notTakeValue = dp[i-1];
+
+            dp[i] = max(takeValue, notTakeValue);
         }
-        int lastElementTake = houseRobber(1, nums, n - 1, dp);
+
+        int firstElementTake = dp[n - 2];
+
+        fill(dp.begin(), dp.end(), -1);
+        dp[1] = nums[1];
+        dp[2] = max(nums[1], nums[2]);
+
+        for(int i=3;i<n;i++){
+            int takeValue = nums[i] + dp[i-2];
+            int notTakeValue = dp[i-1];
+
+            dp[i] = max(takeValue, notTakeValue);
+        }
+
+        int lastElementTake = dp[n-1];
+
 
         return max(firstElementTake, lastElementTake);
     }
