971d4ec4728913ebe71c6d12e22ad1c39ed04f20
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-06-28 22:12:16 +0530

    Time: 0 ms (100%), Space: 10.6 MB (45.89%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -9,16 +9,29 @@ public:
             return dp[index];
         }
 
-        int takeValue = nums[index]  + houseRobber(index + 2, nums, dp);
+        int takeValue = nums[index] + houseRobber(index + 2, nums, dp);
         int notTakeValue = houseRobber(index + 1, nums, dp);
 
         return dp[index] = max(takeValue, notTakeValue);
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
+        if(n == 1){
+            return nums[0];
+        }
         vector<int> dp(n, -1);
-        return houseRobber(0, nums, dp);
+        // return houseRobber(0, nums, dp);
 
-        // return dp[n - 1];
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for(int i=2;i<n;i++){
+            int takeValue = nums[i] + dp[i - 2];
+            int notTakeValue = dp[i - 1];
+
+            dp[i] = max(takeValue, notTakeValue);
+        }
+
+        return dp[n-1];
     }
 };
