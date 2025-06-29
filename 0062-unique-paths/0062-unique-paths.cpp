f797d5c4592f07241243d4e07d969be9137c7d66
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-06-29 16:09:16 +0530

    Time: 2 ms (19.06%), Space: 9.4 MB (33.53%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -18,7 +18,28 @@ public:
         return dp[i][j] = rightSide + downSide;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return solve(0, 0, m, n, dp);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        // return solve(0, 0, m, n, dp);
+
+        // m = 5, n = 1
+        // first col should be 1
+        for(int i=0;i<m;i++){
+            dp[i][0] = 1;
+        }
+
+        // m = 1, n = 5
+        // first row should be 1
+        for(int i=0;i<n;i++){
+            dp[0][i] = 1;
+        }
+
+        for(int i = 1;i<m; i++){
+            for(int j=1;j<n;j++){
+                    int rightSide = dp[i][j - 1];
+                    int downSide = dp[i-1][j];
+                    dp[i][j] = rightSide + downSide;
+                }
+        }
+        return dp[m-1][n-1];
     }
 };
