2f175c7a1f1510e9e0c1df9468f2680cc2f5c4e4
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-06-30 15:36:00 +0530

    Time: 8 ms (21.15%), Space: 18.4 MB (29.77%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
-                          int k) {
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         unordered_map<int, list<pair<int, int>>> adj;
 
-        for (int i = 0; i < flights.size(); i++) {
+        for(int i=0;i<flights.size(); i++){
             int u = flights[i][0];
             int v = flights[i][1];
             int w = flights[i][2];
 
             adj[u].push_back({v, w});
         }
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int,pair<int, int>>>, greater<pair<int, pair<int, int>>>> minHeap;
 
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> q;
         vector<int> stops(n, INT_MAX);
+        minHeap.push({0, {src, 0}});
 
-        q.push({0, src, 0}); // stop, node, dist
-        // stops[src] = 0;
+        while(!minHeap.empty()){
+            auto frontpair = minHeap.top();
+            minHeap.pop();
+            
+            int nodeDistance= frontpair.first;
+            int node = frontpair.second.first;
+            int nodeStop = frontpair.second.second;
 
-        while (!q.empty()) {
-            auto [cost, node, stop] = q.top();
-            q.pop();
-
-            if (node == dst) {
-                return cost;
+            if(node == dst){
+                return nodeDistance;
             }
 
-            if (stop > k || stop >= stops[node]) continue;
-            stops[node] = stop;
+            if(nodeStop > k || nodeStop >= stops[node]) continue;
 
+            stops[node] = nodeStop;
 
-            for (auto neighbour : adj[node]) {
-                int nextNeighbour = neighbour.first;
-                int neighbourCost = neighbour.second + cost;
-                    // q.push({stop + 1, nextNeighbour, neighbourCost});
-                q.push({neighbourCost, nextNeighbour, stop + 1});
+            for(auto neighbour : adj[node]){
+                int neighbourNode = neighbour.first;
+                int neighbourDistance = neighbour.second + nodeDistance;
+
+                minHeap.push({neighbourDistance, {neighbourNode, nodeStop + 1}});
             }
         }
 
