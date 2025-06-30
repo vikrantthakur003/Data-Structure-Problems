0a51d2ab056a1bdf242d54a67d1bf5aa3f398a70
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-06-30 16:46:39 +0530

    Time: 84 ms (60.57%), Space: 46.3 MB (46.24%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        unordered_map<int, list<pair<int, int>> > adj;
+        
+        unordered_map<int, list<pair<int, int>>> adj;
         for(int i=0;i<times.size();i++){
             int u = times[i][0];
             int v = times[i][1];
@@ -11,26 +12,26 @@ public:
         }
 
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > minHeap;
-        vector<int> distance(n+1, INT_MAX);
+        vector<int> distance(n + 1, INT_MAX);
 
-        minHeap.push({k, 0}); // node and distance
+        minHeap.push({0, k});
         distance[k] = 0;
+
 
         while(!minHeap.empty()){
             pair<int, int> frontpair = minHeap.top();
             minHeap.pop();
 
-            int node  = frontpair.first;
-            int nodeDistance = frontpair.second;
+            int nodeDistance = frontpair.first;
+            int node = frontpair.second;
 
             for(auto neighbour : adj[node]){
                 int neighbourNode = neighbour.first;
                 int neighbourDistance = neighbour.second + nodeDistance;
-                cout<<neighbourNode << " " << neighbourDistance<<endl;
 
                 if(neighbourDistance < distance[neighbourNode]){
                     distance[neighbourNode] = neighbourDistance;
-                    minHeap.push({neighbourNode, neighbourDistance});
+                    minHeap.push({neighbourDistance, neighbourNode});
                 }
             }
         }
@@ -38,12 +39,12 @@ public:
         int maxValue = INT_MIN;
         for(int i=1;i<distance.size();i++){
             if(distance[i] == INT_MAX){
-              return -1;
+                return -1;
             } else {
                 maxValue = max(maxValue, distance[i]);
             }
         }
 
-        return maxValue > 0 ? maxValue : -1;
+        return maxValue;
     }
 };
