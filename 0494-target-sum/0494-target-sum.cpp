f040396fc0c587a868f59fcdfc79a724c88fd6c5
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-08 22:13:35 +0530

    Time: 140 ms (36.69%), Space: 11.7 MB (83.3%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -10,10 +10,43 @@ public:
 
         return takeSubstractSign + takePlusSign;
     }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        
-        int n = nums.size();
 
-        return solve(0, 0, nums, target, n);
+    int getSubsetS1(int ind, vector<int>&nums, int target){
+        if(ind == 0){
+            if(target == 0 && nums[0] == 0){
+                return 2;
+            } else if(target == 0 || nums[0] == target){
+                return 1;
+            } 
+            return 0;
+        }
+
+        int takeValue = 0;
+        if(nums[ind] <= target){
+            takeValue = getSubsetS1(ind - 1, nums, target - nums[ind]);
+        }
+
+        int notTakeValue = getSubsetS1(ind - 1, nums, target);
+
+        return takeValue + notTakeValue;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        // return solve(0, 0, nums, target, n);
+
+        // approach like partition with difference
+        int totalSum = 0;
+        for(int i=0;i<n;i++){
+            totalSum += nums[i];
+        }
+
+        if((totalSum - target) < 0){
+            return 0;
+        } else if((totalSum - target)%2 == 1){
+            return 0;
+        }
+
+        int targetValue = (totalSum - target)/2;
+        return getSubsetS1( n - 1, nums, targetValue);
     }
 };
