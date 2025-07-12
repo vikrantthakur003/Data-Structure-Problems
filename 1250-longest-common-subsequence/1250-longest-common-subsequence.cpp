88a2e92e17d5c0b92b2c8e7247c52ea4732f234f
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-12 23:07:55 +0530

    Time: 26 ms (63.47%), Space: 27.5 MB (28.39%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -43,6 +43,13 @@ public:
             }
         }
 
+        // for(int i=0;i<=n;i++){
+        //     for(int j=0;j<=m;j++){
+        //         cout<<dp[i][j]<< " ";
+        //     }
+        //     cout<<endl;
+        // }
+
         return dp[n][m];
     }
 };
