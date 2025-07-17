1496e99d99c480ca76b0a3c175784a24c9ccb728
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-17 22:05:19 +0530

    Time: 30 ms (59.17%), Space: 44.4 MB (14.52%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    const int MOD = 1e9 + 7;
     int solve(int ind1, int ind2, string &s, string &t, vector<vector<int>> &dp){
         if(ind2 >= t.size()){
             return 1;
@@ -26,7 +27,38 @@ public:
         }
     }
     int numDistinct(string s, string t) {
-        vector<vector<int>> dp(s.size(), vector<int>(t.size(), -1));
-        return solve(0, 0, s, t, dp);
+        int n = s.size();
+        int m = t.size();
+        // vector<vector<int>> dp(s.size(), vector<int>(t.size(), -1));
+        // return solve(0, 0, s, t, dp);
+
+        vector<vector<long long>> dp(s.size() + 1, vector<long long>(t.size() + 1, 0));
+
+        // for 0 s
+        for(int i=0; i<=n; i++){
+            dp[i][0] = 1;
+        }
+
+        // for 0 t
+        // for(int i=1; i<=m; i++){
+        //     dp[0][i] = 0;
+        // }
+
+        // dp[0][0] = 1;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m ;j++){
+                if(s[i - 1] == t[j - 1]){
+                    int takeBothValue = dp[i - 1][j - 1];
+                    int takeOneValue = dp[i - 1][j];
+
+                    dp[i][j] = (takeBothValue + takeOneValue) % MOD;
+                } else {
+                    dp[i][j] = (dp[i - 1][j])%MOD;
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
