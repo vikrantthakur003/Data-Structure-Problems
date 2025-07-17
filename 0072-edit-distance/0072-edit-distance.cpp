0150ddaf2fc91703504806d92717ad9d3b411392
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-17 23:32:46 +0530

    Time: 5 ms (71.45%), Space: 13.1 MB (46.02%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -16,7 +16,7 @@ public:
             return dp[ind1][ind2] = solve(ind1 + 1, ind2 + 1, s1, s2, dp);
         } else {
             // replacemenet
-            int replareCharacter = 1 + solve(ind1 + 1, ind2 + 1, s1, s2, dp);
+            int replaceCharacter = 1 + solve(ind1 + 1, ind2 + 1, s1, s2, dp);
 
             // removing
             int removeCharacter = 1 + solve(ind1 + 1, ind2, s1, s2, dp);
@@ -24,13 +24,43 @@ public:
             // insertion
             int insertCharacter = 1 + solve(ind1, ind2 + 1, s1, s2, dp);
 
-            return dp[ind1][ind2] = min(replareCharacter, min(removeCharacter, insertCharacter));
+            return dp[ind1][ind2] = min(replaceCharacter, min(removeCharacter, insertCharacter));
         }
     }
     int minDistance(string word1, string word2) {
+
+        if(word1 == word2){
+            return 0;
+        }
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(0, 0, word1, word2, dp);
+        // vector<vector<int>> dp(n, vector<int>(m, -1));
+        // return solve(0, 0, word1, word2, dp);
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for(int i= 0; i<=n; i++){
+            dp[i][0] = i;
+        }
+
+        for(int i=0; i<=m; i++){
+            dp[0][i] = i;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1; j<=m; j++){
+                if(word1[i - 1] == word2[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int replaceCharacter = 1 + dp[i - 1][j - 1];
+                    int removeCharacter = 1 + dp[i - 1][j];
+                    int insertCharacter = 1 + dp[i][j - 1];
+
+                    dp[i][j] = min(replaceCharacter, min(removeCharacter, insertCharacter));
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
