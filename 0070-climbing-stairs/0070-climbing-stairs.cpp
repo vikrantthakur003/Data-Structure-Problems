da8067efc6472498360c203d6209b2ae1d1684da
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-19 11:09:43 +0530

    Time: 0 ms (100%), Space: 8.6 MB (36.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,40 +1,16 @@
 class Solution {
 public:
-    int solve(int n, vector<int> &dp){ // tabulation
+    int solve(int n, vector<int> &dp){
         if(n <= 1){
             return 1;
         }
         if(dp[n] != -1){
             return dp[n];
         }
-
-        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+        return dp[n]  = solve(n - 1, dp) + solve(n - 2, dp);
     }
     int climbStairs(int n) {
-        if(n <= 1){
-            return n;
-        }
         vector<int> dp(n + 1, -1);
-        // solve(n, dp);
-
-        // tabulation
-        // dp[0]= 1;
-        // dp[1] = 1;
-
-        // for(int i=2;i<=n;i++){
-        //     dp[i] = dp[i-1] + dp[i-2];
-        // }
-
-        // optimization
-        int prev2 = 1;
-        int prev = 1;
-        int curr;
-        for(int i=2;i<=n;i++){
-            curr = prev + prev2;
-            prev2 = prev;
-            prev = curr;
-        }
-        return curr;
-        // return dp[n];
+        return solve(n, dp);
     }
 };
