d4d2a7d26d8468697e9bcec8c5d1a238323bee89
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-19 12:40:23 +0530

    Time: 0 ms (100%), Space: 10.9 MB (39.95%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -13,19 +13,54 @@ public:
 
         return dp[index] = max(takeHouse, skipHouse);
     }
+
     int rob(vector<int>& nums) {
         int n = nums.size();
 
         if(n == 1){
             return nums[0];
         }
-        vector<int> dp(n, -1);
+        if(n == 2){
+            return max(nums[0], nums[1]);
+        }
+        // vector<int> dp(n, -1);
 
-        int skipFirstHouse = solve(1, n, nums, dp);
-        fill(dp.begin(), dp.end(), -1);
+        // int skipFirstHouse = solve(1, n, nums, dp);
+        // fill(dp.begin(), dp.end(), -1);
 
-        int skipLastHouse = solve(0, n - 1, nums, dp);
+        // int skipLastHouse = solve(0, n - 1, nums, dp);
 
-        return max(skipFirstHouse, skipLastHouse);
+        // return max(skipFirstHouse, skipLastHouse);
+
+        vector<int> dp(n, 0);
+
+        // only take first house
+        dp[0] = nums[0];
+        dp[1] = max(nums[1], nums[0]);
+
+        for(int i=2; i<n-1; i++){
+            int takeHouse = nums[i] + dp[i - 2];
+            int skipHouse = dp[i - 1];
+
+            dp[i] = max(takeHouse, skipHouse);
+        }
+
+        int takefirstHouse = dp[n - 2];
+
+        fill(dp.begin(), dp.end(), 0);
+
+        dp[1] = nums[1];
+        dp[2] = max(nums[1], nums[2]);
+
+        for(int i=3;i<n;i++){
+            int takeHouse = nums[i] + dp[i - 2];
+            int skipHouse = dp[i - 1];
+
+            dp[i] = max(takeHouse, skipHouse);
+        }
+
+        int takeLastHouse = dp[n - 1];
+
+        return max(takefirstHouse, takeLastHouse);
     }
 };
