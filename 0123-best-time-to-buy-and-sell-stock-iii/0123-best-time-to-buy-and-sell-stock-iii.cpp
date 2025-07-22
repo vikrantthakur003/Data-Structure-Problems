87f65e50ab7dbfb16fc3a2c6a901ec3d2fece905
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-22 21:41:58 +0530

    Time: 456 ms (19.26%), Space: 223.6 MB (33.41%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    int solve(int ind, bool canIBuy, int noOfTimesBuy, vector<int> & prices, vector<vector<vector<int>>>& dp){
+    int solve(int ind, bool canIBuy, int noOfTimesTransactions, vector<int> & prices, vector<vector<vector<int>>>& dp){
         if(ind >= prices.size()){
             return 0;
         }
 
-        if(noOfTimesBuy > 2){
+        if(noOfTimesTransactions >= 2){
             return 0;
         }
 
-        if(dp[ind][canIBuy][noOfTimesBuy] != -1){
-            return dp[ind][canIBuy][noOfTimesBuy];
+        if(dp[ind][canIBuy][noOfTimesTransactions] != -1){
+            return dp[ind][canIBuy][noOfTimesTransactions];
         }
 
         int profit = 0;
-        if(canIBuy && noOfTimesBuy <= 2){
-            int buyValue = -prices[ind] + solve(ind + 1, false, noOfTimesBuy + 1, prices, dp);
-            int skipBuy = solve(ind + 1, true, noOfTimesBuy, prices, dp);
+        if(canIBuy && noOfTimesTransactions <= 2){
+            int buyValue = -prices[ind] + solve(ind + 1, false, noOfTimesTransactions, prices, dp);
+            int skipBuy = solve(ind + 1, true, noOfTimesTransactions, prices, dp);
 
             profit = max(buyValue, skipBuy);
-        } else if(noOfTimesBuy <=2 ) {
-            int sellValue = prices[ind] + solve(ind + 1, true, noOfTimesBuy, prices, dp);
-            int skipSell = solve(ind + 1, false, noOfTimesBuy, prices, dp);
+        } else if(noOfTimesTransactions <=2) {
+            int sellValue = prices[ind] + solve(ind + 1, true, noOfTimesTransactions + 1, prices, dp);
+            int skipSell = solve(ind + 1, false, noOfTimesTransactions, prices, dp);
 
             profit = max(sellValue, skipSell);
         }
 
-        return dp[ind][canIBuy][noOfTimesBuy] = profit;
+        return dp[ind][canIBuy][noOfTimesTransactions] = profit;
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(2, -1)));
         return solve(0, 1, 0, prices, dp);
     }
 };
