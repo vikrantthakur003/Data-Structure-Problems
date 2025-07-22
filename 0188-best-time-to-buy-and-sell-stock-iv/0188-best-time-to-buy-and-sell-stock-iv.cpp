e79a3219a660ab6eda932e29d407c5c91e24b0d0
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-22 22:48:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -30,7 +30,35 @@ public:
     }
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>> (2, vector<int>(k + 1, -1)));
-        return solve(0, 1, k, prices, dp);
+        // vector<vector<vector<int>>> dp(n, vector<vector<int>> (2, vector<int>(k + 1, -1)));
+        // return solve(0, 1, k, prices, dp);
+
+        vector<vector<vector<int>>> dp(
+            n + 1, vector<vector<int>>(2, vector<int>(k + 1, 0)));
+
+        for (int i = n-1; i >= 0; i--) {
+            for (int canyIBuy = 1; canyIBuy >= 0; canyIBuy--) {
+                for (int trans = 0; trans <= k; trans++) {
+
+                    if (canyIBuy) {
+                        int buyValue = -prices[i] + dp[i + 1][0][trans];
+                        int skipBuy = dp[i + 1][1][trans];
+
+                        dp[i][canyIBuy][trans] = max(buyValue, skipBuy);
+                    } else {
+                        if (trans < 2) {
+                            int sellValue = prices[i] + dp[i + 1][1][trans + 1];
+                            int skipSell = dp[i + 1][0][trans];
+
+                            dp[i][canyIBuy][trans] = max(sellValue, skipSell);
+                        } else {
+                            dp[i][canyIBuy][trans] = dp[i + 1][0][trans];
+                        }
+                    }
+                }
+            }
+        }
+
+        return dp[0][1][0];
     }
 };
