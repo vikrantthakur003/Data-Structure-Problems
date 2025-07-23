293552fc22e4ff0a43cd38944eeab37b167d9d87
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-23 18:57:48 +0530

    Time: 0 ms (100%), Space: 11.9 MB (54.31%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,67 +1,30 @@
 class Solution {
 public:
-    int solve(int i, int j, int m, int n, vector<vector<int>>& obstacleGrid,
-              vector<vector<int>>& dp) {
-        if (i >= m || j >= n) {
+    int solve(int ind1, int ind2, vector<vector<int>>&arr, vector<vector<int>> &dp){
+        if(ind1 >= arr.size() || ind2 >= arr[ind1].size()){
             return 0;
         }
 
-        if (i == m - 1 && j == n - 1) {
-            if (obstacleGrid[i][j] == 0) {
-                return 1;
-            } else {
-                return 0;
-            }
+        if(arr[ind1][ind2] == 1){
+            return 0;
         }
 
-        if (dp[i][j] != -1) {
-            return dp[i][j];
+        if(dp[ind1][ind2] != -1){
+            return dp[ind1][ind2];
         }
 
-        int rightSide = 0;
-        int downSide = 0;
-        if (j + 1 < n && obstacleGrid[i][j + 1] == 0) {
-            rightSide = solve(i, j + 1, m, n, obstacleGrid, dp);
+        if(ind1 == arr.size() - 1 && ind2 == arr[ind1].size() - 1){
+            return 1;
         }
+        int rightSide = solve(ind1 + 1, ind2, arr, dp);
+        int downSide = solve(ind1, ind2 + 1, arr, dp);
 
-        if (i + 1 < m && obstacleGrid[i + 1][j] == 0) {
-            downSide = solve(i + 1, j, m, n, obstacleGrid, dp);
-        }
-
-        return dp[i][j] = rightSide + downSide;
+        return dp[ind1][ind2] = rightSide + downSide;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-
-        // vector<vector<int>> dp(m, vector<int>(n, -1));
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        if (obstacleGrid[0][0] == 1) {
-            return 0;
-        } else {
-            dp[0][0] = 1;
-        }
-
-        // return solve(0, 0, m, n, obstacleGrid, dp);
-
-        // tabulation
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
-                } else {
-                    if (j > 0) {
-                        dp[i][j] += dp[i][j - 1];
-                    }
-
-                    if (i > 0) {
-                        dp[i][j] += dp[i - 1][j];
-                    }
-                }
-            }
-        }
-
-        return dp[m - 1][n - 1];
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(0, 0, obstacleGrid, dp);
     }
 };
