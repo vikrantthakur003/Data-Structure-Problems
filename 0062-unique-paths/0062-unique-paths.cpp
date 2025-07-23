286d21494db4406a5b9fe68c660cf9378c403ba0
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-23 18:32:49 +0530

    Time: 0 ms (100%), Space: 9.3 MB (57.46%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,45 +1,23 @@
 class Solution {
 public:
-    int solve(int i, int j, int m, int n, vector<vector<int>> & dp){
-        if(i >= m || j >= n){
+    int solve(int ind1, int ind2, int m, int n, vector<vector<int>> &dp){
+        if(ind1 >= m || ind2 >= n){
             return 0;
         }
-        if(i == m - 1 && j == n - 1){
+        if(ind1 == m-1 && ind2 == n-1){
             return 1;
         }
 
-        if(dp[i][j] != -1){
-            return dp[i][j];
+        if(dp[ind1][ind2] != -1){
+            return dp[ind1][ind2];
         }
+        int rightSide = solve(ind1 + 1, ind2, m, n, dp);
+        int downSide = solve(ind1, ind2 + 1, m, n, dp);
 
-        int rightSide = solve(i, j + 1, m, n, dp);
-        int downSide = solve(i + 1, j, m, n, dp);
-
-        return dp[i][j] = rightSide + downSide;
+        return dp[ind1][ind2] = rightSide + downSide;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        // return solve(0, 0, m, n, dp);
-
-        // m = 5, n = 1
-        // first col should be 1
-        for(int i=0;i<m;i++){
-            dp[i][0] = 1;
-        }
-
-        // m = 1, n = 5
-        // first row should be 1
-        for(int i=0;i<n;i++){
-            dp[0][i] = 1;
-        }
-
-        for(int i = 1;i<m; i++){
-            for(int j=1;j<n;j++){
-                    int rightSide = dp[i][j - 1];
-                    int downSide = dp[i-1][j];
-                    dp[i][j] = rightSide + downSide;
-                }
-        }
-        return dp[m-1][n-1];
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(0, 0, m, n, dp);
     }
 };
