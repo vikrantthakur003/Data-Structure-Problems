4b7ea1224e36980ef7e2b8ceb775fbc947e200c6
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-26 17:55:57 +0530

    Time: 0 ms (100%), Space: 16.3 MB (61.86%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -22,7 +22,30 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return solve(0, 0, grid, m, n, dp);
+        // vector<vector<int>> dp(m, vector<int>(n, -1));
+        // return solve(0, 0, grid, m, n, dp);
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        
+        dp[0][0] = grid[0][0];
+        for(int i = 1; i<n; i++){
+            dp[0][i] = grid[0][i] + dp[0][i - 1];
+        }
+
+        for(int i = 1; i<m; i++){
+            dp[i][0] += grid[i][0] + dp[i - 1][0];
+        }
+
+        for(int i = 1; i<m; i++){
+            for(int j = 1; j<n; j++){
+                int rightSide = grid[i][j] + dp[i - 1][j];
+                int downSide = grid[i][j] + dp[i][j - 1];
+
+                dp[i][j] = min(rightSide, downSide);
+            }
+        }
+
+        return dp[m - 1][n - 1];
+        
     }
 };
