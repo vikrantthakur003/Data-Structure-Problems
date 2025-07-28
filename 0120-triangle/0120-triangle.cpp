be1550c61049e183f31c7e71444f39efb7235bc5
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-28 18:20:59 +0530

    Time: 0 ms (100%), Space: 13.4 MB (14.6%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,50 +1,45 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& triangle,
-              vector<vector<int>>& dp) {
-        if (i >= triangle.size()) {
-            return 0;
+    int solve(int ind1, int ind2, vector<vector<int>> & arr, vector<vector<int>> &dp){
+        if(ind1 == arr.size() - 1){
+            return arr[ind1][ind2];
         }
 
-        if (dp[i][j] != -1) {
-            return dp[i][j];
+        if(dp[ind1][ind2] != -1){
+            return dp[ind1][ind2];
         }
 
-        int takeSameIndex = solve(i + 1, j, triangle, dp);
-        int takeNextIndex = solve(i + 1, j + 1, triangle, dp);
+        int sameIndex = solve(ind1 + 1, ind2, arr, dp);
+        int nextIndex = solve(ind1 + 1, ind2 + 1, arr, dp);
 
-        return dp[i][j] = triangle[i][j] + min(takeSameIndex, takeNextIndex);
+        return dp[ind1][ind2] =  arr[ind1][ind2] + min(sameIndex, nextIndex);
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        int m = triangle[n - 1].size();
+        int m = triangle[n-1].size();
         // vector<vector<int>> dp(n, vector<int>(m, -1));
         // return solve(0, 0, triangle, dp);
 
-        vector<vector<int>> dp(n);
-
-        for (int i = 0; i < n; i++) {
-            dp[i].resize(triangle[i].size(), 0);
-        }
+        vector<vector<int>> dp(n, vector<int>(m, 0));
 
         dp[0][0] = triangle[0][0];
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < triangle[i].size(); j++) {
-                int takeSameIndex = INT_MAX;
-                int takeNextIndex = INT_MAX;
+        for(int i = 1; i<n; i++){
+            for(int j = 0; j<triangle[i].size(); j++){
+                int sameIndex = INT_MAX;
+                int nextIndex = INT_MAX;
 
-                if (j < triangle[i - 1].size()){
-                    takeSameIndex = dp[i - 1][j];
+                if(j < triangle[i - 1].size()){
+                   sameIndex = dp[i - 1][j]; 
                 }
-                if (j > 0) {
-                    takeNextIndex = dp[i - 1][j - 1];
+                if(j > 0 ){
+                    nextIndex = dp[i - 1][j - 1];
                 }
-
-                dp[i][j] = triangle[i][j] + min(takeSameIndex, takeNextIndex);
+                
+                dp[i][j] = triangle[i][j] + min(sameIndex, nextIndex);
             }
         }
 
-        return *min_element(dp[n - 1].begin(), dp[n - 1].end());
+        return *min_element(dp[n-1].begin(), dp[n-1].end());
     }
 };
