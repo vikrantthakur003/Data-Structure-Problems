ea3f3e9e375240419743995358f959a5b6c04b27
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-29 23:08:28 +0530

    Time: 806 ms (5.02%), Space: 13.9 MB (58.1%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -22,7 +22,32 @@ public:
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
 
-        vector<vector<int>>dp(n + 2, vector<int>(n + 2, -1));
-        return solve(1, n, nums, dp);
+        int c = nums.size();
+
+        // vector<vector<int>>dp(n + 2, vector<int>(n + 2, -1));
+        // return solve(1, n, nums, dp);
+
+        vector<vector<int>>dp(c, vector<int>(c, 0));
+
+        for(int i = n; i >= 1; i--){
+            for(int j = i; j <= n; j++){
+                int maxValue = INT_MIN;
+                for(int k = i; k <= j; k++){
+                    int cost = nums[i - 1] * nums[k] * nums[j + 1] + dp[i][k - 1] + dp[k + 1][j];
+                    maxValue = max(maxValue, cost);
+                }
+
+                dp[i][j] = maxValue;
+            }
+        }
+
+        for(int i = 0; i< c; i++){
+            for(int j = 0; j< c; j++){
+                cout<<dp[i][j]<< " ";
+            }
+            cout<<endl;
+        }
+
+        return dp[1][n];
     }
 };
