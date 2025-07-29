1869f8a0cd35cf1d8bf2a9bf8d6f28da1af10d40
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-29 17:45:18 +0530

    Time: 206 ms (5.71%), Space: 15.1 MB (38.88%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,71 +1,69 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp){
-        if(i >=matrix.size()  || j >= matrix.size()){
+    int solve(int ind1, int ind2, vector<vector<int>> &arr, vector<vector<int>> &dp){
+        if(ind1 >= arr.size() || ind2 >= arr.size()){
             return INT_MAX;
         }
 
-        if(i == matrix.size() - 1){
-            return matrix[i][j];
+        if(ind1 == arr.size() - 1){
+            return arr[ind1][ind2];
         }
 
-        if(dp[i][j] != -1){
-            return dp[i][j];
+        if(dp[ind1][ind2] != -1){
+            return dp[ind1][ind2];
         }
 
-        int takeSameIndex = solve(i+1, j, matrix, dp);
-        int takeNextIndex = solve(i+1, j+1, matrix, dp);
-        int takePreviousIndex = solve(i+1, j-1, matrix, dp);
+        int sameIndex = solve(ind1 + 1, ind2, arr, dp);
+        int leftIndex = solve(ind1 + 1, ind2 - 1, arr, dp);
+        int rightIndex = solve(ind1 + 1, ind2 + 1, arr, dp);
 
-
-        return dp[i][j] = matrix[i][j] + min(takeSameIndex, min(takeNextIndex, takePreviousIndex));
+        return dp[ind1][ind2] = arr[ind1][ind2] + min(sameIndex, min(leftIndex, rightIndex));
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        // int minValue = INT_MAX;
+        int minValue = INT_MAX;
         int n = matrix.size();
         // vector<vector<int>> dp(n, vector<int>(n, -1));
-
-        // for(int i=0;i<matrix.size();i++){
+        // for(int i = 0; i<matrix.size(); i++){
         //     minValue = min(minValue, solve(0, i, matrix, dp));
         // }
-        
+
         // return minValue;
 
-
-        // Tabulation
         vector<vector<int>> dp(n, vector<int>(n, 0));
-        for(int i=0;i<n;i++){
-            dp[0][i] = matrix[0][i];
-        }
 
-        for(int i=1;i<n;i++){
-            for(int j=0;j<n;j++){
-                int takeSameIndex = INT_MAX;
-                int takePreviousIndex = INT_MAX;
-                int takeNextIndex = INT_MAX;
-                if(i > 0){
-                    takeSameIndex = dp[i - 1][j];
+        for(int ind =  n - 1; ind >= 0; ind--){
+            for(int i = n - 1;i >= 0; i--){
+                for(int j = ind; j >= 0; j--){
+                    int sameIndex = INT_MAX;
+                    int nextIndex = INT_MAX;
+                    int previousIndex = INT_MAX;
+                    if (i < n - 1){
+                        sameIndex = dp[i + 1][j];
+                    }
+                    if(i < n - 1 && j < n - 1){
+                        nextIndex = dp[i + 1][j + 1];  
+                    }
+                    if (i < n - 1 && j > 0 ){
+                        previousIndex  = dp[i + 1][j - 1];
+                    }
+
+                    int minValue = min(sameIndex, min(nextIndex, previousIndex));
+                    if(minValue == INT_MAX){
+                         dp[i][j] = matrix[i][j];
+                    } else {
+                         dp[i][j] = matrix[i][j] + minValue;
+                    }
                 }
-                if(i > 0 && j > 0){
-                    takePreviousIndex = dp[i - 1][j - 1];
-                }
-
-                if(i > 0 && j < n - 1){
-                    takeNextIndex = dp[i - 1][j + 1];
-                }
-
-                int minValue = min(takeSameIndex, min(takePreviousIndex, takeNextIndex));
-
-                dp[i][j] = matrix[i][j] + minValue;
             }
         }
 
-
-        int minValue = INT_MAX;
-        for(int i=0;i<n;i++){
-            minValue = min(minValue, dp[n-1][i]);
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                cout<<dp[i][j]<< " ";
+            }
+            cout<<endl;
         }
 
-        return minValue;
+        return *min_element(dp[0].begin(), dp[0].end());
     }
 };
