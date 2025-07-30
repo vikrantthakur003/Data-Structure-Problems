4d8ae45786f5162b70a3186c3156ce46f4927030
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-30 22:08:36 +0530

    Time: 27 ms (9.96%), Space: 12.4 MB (87.05%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int solve(int i, int k, vector<int>&arr, vector<int>& dp){
-        if(i >= arr.size()){
+    int solve(int i, int k, vector<int>& arr, vector<int>& dp) {
+        if (i >= arr.size()) {
             return 0;
         }
 
-        if(dp[i] != -1){
+        if (dp[i] != -1) {
             return dp[i];
         }
 
         int maxAns = 0;
         int maxValue = 0;
-        for(int j = i; j < min(i + k, (int)arr.size()); j++){
+        for (int j = i; j < min(i + k, (int)arr.size()); j++) {
             maxValue = max(maxValue, arr[j]);
             int length = j - i + 1;
 
@@ -23,7 +23,28 @@ public:
         return dp[i] = maxAns;
     }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        vector<int> dp(arr.size(), -1);
-        return solve(0, k, arr, dp);
+        // vector<int> dp(arr.size(), -1);
+        // return solve(0, k, arr, dp);
+
+        int n = arr.size();
+        vector<int> dp(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            int maxValue = 0;
+            int maxAns = 0;
+            for (int j = i; j < min(i + k, (int)arr.size()); j++) {
+                maxValue = max(maxValue, arr[j]);
+                int length = j - i + 1;
+                int cost = maxValue * length + dp[j + 1];
+
+                maxAns = max(maxAns, cost);
+            }
+            dp[i] = maxAns;
+        }
+        for (int i = 0; i < n; i++) {
+            cout << dp[i] << " ";
+        }
+
+        return dp[0];
     }
 };
