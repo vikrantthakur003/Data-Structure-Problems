6c6c418e93b055fa12f3aef74b33012341d04dbc
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-30 19:06:15 +0530

    Time: 167 ms (24.29%), Space: 110.6 MB (24.62%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,63 +1,41 @@
 class Solution {
 public:
-    bool solve(int index, vector<int>& nums, int target, vector<vector<int>> &dp){
-        if(target == 0){
-            return true;
-        }
-        if(index == nums.size()){
-            return target == 0;
-        }
-
-        if(dp[index][target] != -1){
-            return dp[index][target];
-        }
-
-        bool takeValue = false;
-        if(nums[index] <= target){
-            takeValue = solve(index + 1, nums, target - nums[index], dp);
-        }
-        
-        bool notTakeValue = solve(index + 1, nums, target, dp);
-
-        return dp[index][target] = takeValue || notTakeValue;
-    }
-    bool canPartition(vector<int>& nums) {
-        int totalSum = 0;
-        for(int i=0;i<nums.size();i++){
-            totalSum += nums[i];
-        }
-
-        if(totalSum%2 != 0){
+    int solve(int ind, int target, vector<int>& nums, vector<vector<int>> &dp) {
+        if (ind >= nums.size()) {
+            if (target == 0) {
+                return true;
+            }
             return false;
         }
-        int targetSum = totalSum/2;
-        // vector<vector<int>> dp(nums.size(), vector<int>(targetSum + 1, -1));
-        // return solve(0, nums, targetSum, dp);
 
-        vector<vector<bool>> dp(nums.size(), vector<bool>(targetSum + 1, false));
-
-        // if n > 0 but targetSum == 0 then avery row have true value
-        for(int i=0;i<nums.size();i++){
-            dp[i][0] = true;
+        if (target == 0) {
+            return true;
         }
 
-        if(nums[0] <= targetSum){
-            dp[0][nums[0]] = true;
+        if(dp[ind][target] != -1){
+            return dp[ind][target];
+        }
+        int takeValue = false;
+        if (nums[ind] <= target) {
+            takeValue = solve(ind + 1, target - nums[ind], nums, dp);
+        }
+        int notTakeValue = solve(ind + 1, target, nums, dp);
+
+        return dp[ind][target] = takeValue || notTakeValue;
+    }
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
         }
 
-        for(int i=1;i<nums.size();i++){
-            for(int target = 1; target <= targetSum; target++){
-                bool notTakeValue = dp[i - 1][target];
-                bool takeValue = false;
-
-                if(nums[i] <= target){
-                    takeValue = dp[i - 1][target - nums[i]];
-                }
-
-                dp[i][target] = notTakeValue || takeValue;
-            }
+        if (sum % 2 != 0) {
+            return false;
         }
 
-        return dp[nums.size() - 1][targetSum];
+        int targetSum = sum / 2;
+        vector<vector<int>>dp(n, vector<int>(targetSum + 1, -1));
+        return solve(0, targetSum, nums, dp);
     }
 };
