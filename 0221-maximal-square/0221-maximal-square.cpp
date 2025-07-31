0f918e46239838fc01213a13fa30aff5874ab0f6
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-31 23:46:42 +0530

    Time: 5 ms (80.31%), Space: 30.9 MB (77.41%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -21,13 +21,43 @@ public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>>dp(n, vector<int>(m, -1));
+        // vector<vector<int>>dp(n, vector<int>(m, -1));
 
         int maxSide = 0;
-        for(int i =0; i<n; i++){
-            for(int j =0; j<m; j++){
-                int value = solve(i, j, matrix, dp);
-                maxSide = max(maxSide, value);
+        // for(int i =0; i<n; i++){
+        //     for(int j =0; j<m; j++){
+        //         int value = solve(i, j, matrix, dp);
+        //         maxSide = max(maxSide, value);
+        //     }
+        // }
+
+        vector<vector<int>>dp(n, vector<int>(m, 0));
+        for(int i = 0; i<m; i++){
+            if(matrix[0][i] == '1'){
+                dp[0][i] = 1;
+
+                maxSide = max(maxSide, dp[0][i]);
+            }
+        }
+
+         for(int i = 0; i<n; i++){
+            if(matrix[i][0] == '1'){
+                dp[i][0] = 1;
+
+                maxSide = max(maxSide, dp[i][0]);
+            }
+        }
+
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<m; j++){
+                if(matrix[i][j] == '1'){
+                    int right = dp[i][j - 1];
+                    int down = dp[i - 1][j];
+                    int diagonal = dp[i - 1][j - 1];
+
+                    dp[i][j] = 1 + min({right, down, diagonal});
+                    maxSide = max(maxSide, dp[i][j]);
+                }
             }
         }
 
