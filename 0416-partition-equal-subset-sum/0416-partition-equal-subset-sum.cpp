dac3b0df2ce3af4bff50a853d71b0bcda9742d2c
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-07-31 12:28:55 +0530

    Time: 170 ms (22.47%), Space: 17.2 MB (70.99%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -35,7 +35,32 @@ public:
         }
 
         int targetSum = sum / 2;
-        vector<vector<int>>dp(n, vector<int>(targetSum + 1, -1));
-        return solve(0, targetSum, nums, dp);
+        // vector<vector<int>>dp(n, vector<int>(targetSum + 1, -1));
+        // return solve(0, targetSum, nums, dp);
+
+        vector<vector<bool>>dp(n, vector<bool>(targetSum + 1, false));
+
+        for(int i = 0; i<n; i++){
+            dp[i][0] = true;
+        }
+
+        if(nums[0] <= targetSum){
+            dp[0][nums[0]] = true;
+        }
+        
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<=targetSum; j++){
+                bool takeValue = false;
+                if(nums[i] <= j){
+                    takeValue = dp[i - 1][j - nums[i]];
+                }
+
+                int notTakeValue = dp[i - 1][j];
+
+                dp[i][j] = takeValue || notTakeValue;
+            }
+        }
+
+        return dp[n - 1][targetSum];
     }
 };
