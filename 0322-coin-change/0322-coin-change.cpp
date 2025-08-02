b501f19c969896cd48a6e04d085d7180dd9bf145
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-08-02 20:20:04 +0530

    Time: 51 ms (25.72%), Space: 48.8 MB (43.86%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int solve(int i, int amount, vector<int>&arr, vector<vector<int>>&dp){
-        if(i == 0){
-            if(amount % arr[0] == 0){
+    int solve(int i, int amount, vector<int>& arr, vector<vector<int>>& dp) {
+        if (i == 0) {
+            if (amount % arr[0] == 0) {
                 return amount / arr[0];
             }
             return 1e9;
         }
 
-        if(dp[i][amount] != -1){
+        if (dp[i][amount] != -1) {
             return dp[i][amount];
         }
 
         int takeValue = 1e9;
-        if(arr[i] <= amount){
+        if (arr[i] <= amount) {
             takeValue = 1 + solve(i, amount - arr[i], arr, dp);
         }
 
@@ -22,16 +22,30 @@ public:
         return dp[i][amount] = min(takeValue, notTakeValue);
     }
     int coinChange(vector<int>& coins, int amount) {
-        if(amount == 0){
+        if (amount == 0) {
             return 0;
         }
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        int ans = solve(n - 1, amount, coins, dp);
 
-        if(ans == 1e9){
-            return -1;
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 1e9));
+        for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0) {
+                dp[0][i] = i / coins[0];
+            }
         }
-        return ans;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                int takeValue = 1e9;
+                if (coins[i] <= j) {
+                    takeValue = dp[i][j - coins[i]] + 1;
+                }
+                int notTakeValue = dp[i - 1][j];
+
+                dp[i][j] = min(takeValue, notTakeValue);
+            }
+        }
+
+        return dp[n - 1][amount] == 1e9 ? -1 : dp[n - 1][amount];
     }
 };
