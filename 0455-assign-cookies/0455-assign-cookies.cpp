b3bea70ad586faa2b070f3d0ef3a963cef916e11
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-08-02 13:08:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<int>&g, vector<int>& s, int n, int m){
-        if(i >= n){
-            return 0;
-        }
-        if(j >= m){
+    int solve(int i, int j, vector<int>&g, vector<int>&s, vector<vector<int>> &dp){
+        if(j >= s.size() || i >= g.size()){
             return 0;
         }
 
-        int takeValue = 0;
-        int notTakeValue = 0;
-        if(s[j] >= g[i]){
-            takeValue = 1 + solve(i + 1, j + 1, g, s, n, m);
-        } else {
-            notTakeValue = solve(i, j + 1, g, s, n, m);
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
-    
-        return max(takeValue, notTakeValue);
+        int takeValue = INT_MIN;
+        if(s[j] >= g[i]){
+            takeValue  = 1 + solve(i + 1, j + 1, g, s, dp);
+        }
+        int notTakeValue = solve(i, j + 1, g, s, dp);
+
+        return dp[i][j] = max(takeValue, notTakeValue);
     }
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int n = g.size();
-        int m = s.size();
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
+        int n = g.size();
+        int m = s.size();
+    //     vector<vector<int>> dp(n, vector<int>(m, -1));
+    //    return solve(0, 0, g, s, dp);
 
-        // return solve(0, 0, g, s, n, m);
-        int i = 0;
-        int j = 0;
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
-        while(i<n && j<m){
-            if(s[j] >= g[i]){
-                i++;
+        for(int i = n - 1; i>= 0; i--){
+            for(int j = m - 1; j>=0; j--){
+                int takeValue = INT_MIN;
+                if(s[j] >= g[i]){
+                    takeValue = dp[i + 1][j + 1] + 1;
+                }
+                int notTakeValue = dp[i][j + 1];
+
+                dp[i][j] = max(takeValue, notTakeValue);
             }
-            j++;
         }
 
-        return i;
+        return dp[0][0];
     }
 };
