a48bd7da47f8e4428f88e4913bd742205dd14a7f
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-08-02 13:10:21 +0530

    Time: 12 ms (31.5%), Space: 44.9 MB (43.53%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -24,20 +24,34 @@ public:
     //     vector<vector<int>> dp(n, vector<int>(m, -1));
     //    return solve(0, 0, g, s, dp);
 
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        // vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
-        for(int i = n - 1; i>= 0; i--){
-            for(int j = m - 1; j>=0; j--){
-                int takeValue = INT_MIN;
-                if(s[j] >= g[i]){
-                    takeValue = dp[i + 1][j + 1] + 1;
-                }
-                int notTakeValue = dp[i][j + 1];
+        // for(int i = n - 1; i>= 0; i--){
+        //     for(int j = m - 1; j>=0; j--){
+        //         int takeValue = INT_MIN;
+        //         if(s[j] >= g[i]){
+        //             takeValue = dp[i + 1][j + 1] + 1;
+        //         }
+        //         int notTakeValue = dp[i][j + 1];
 
-                dp[i][j] = max(takeValue, notTakeValue);
+        //         dp[i][j] = max(takeValue, notTakeValue);
+        //     }
+        // }
+
+        // return dp[0][0];
+
+        int i = 0;
+        int j = 0;
+        int ans = 0;
+        while(i <n && j<m){
+            if(s[j] >= g[i]){
+                ans++;
+                i++;
+                j++;
+            } else {
+                j++;
             }
         }
-
-        return dp[0][0];
+        return ans;
     }
 };
