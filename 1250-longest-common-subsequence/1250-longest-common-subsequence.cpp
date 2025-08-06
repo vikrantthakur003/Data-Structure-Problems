8a2cb9923c93ab03b613796e04368cac46e8420a
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-08-06 18:22:55 +0530

    Time: 43 ms (23.21%), Space: 27.6 MB (14.83%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,55 +1,30 @@
 class Solution {
 public:
-    int solve(int ind1, int ind2, string str1, string str2, vector<vector<int>> &dp){
-        if(ind1 >= str1.length() || ind2 >= str2.length()){
+    int solve(int i, int j, string &text1, string &text2, vector<vector<int>> &dp){
+        if(i >= text1.size() || j>=text2.size()){
             return 0;
         }
-
-        if(dp[ind1][ind2] != -1){
-            return dp[ind1][ind2];
+        if(dp[i][j] != -1 ){
+            return dp[i][j];
         }
-        int takeBothString = INT_MIN;
-        if(str1[ind1] == str2[ind2]){
-            return dp[ind1][ind2] = 1 + solve(ind1 + 1, ind2 + 1, str1, str2, dp);
+
+        int takeValue = 0;
+        int notTakeValue = 0;
+        if(text1[i] == text2[j]){
+            takeValue = 1 + solve(i + 1, j + 1, text1, text2, dp);
         } else {
-            return dp[ind1][ind2] = max(solve(ind1 + 1, ind2, str1, str2, dp), solve(ind1, ind2 + 1, str1, str2, dp));
+            int skipFromFirstStr = solve(i + 1, j, text1, text2, dp);
+            int skipFromSecondStr = solve(i, j + 1, text1, text2, dp);
+
+            notTakeValue = max(skipFromFirstStr, skipFromSecondStr);
         }
 
+        return dp[i][j] = max(takeValue, notTakeValue);
     }
     int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.length();
-        int m = text2.length();
-
-        // vector<vector<int>>dp(n, vector<int>(m, -1));
-        // return solve(0, 0, text1, text2, dp);
-
-        vector<vector<int>>dp(n + 1, vector<int>(m + 1, 0));
-
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 0;
-        }
-
-        for(int i=0;i<=m;i++){
-            dp[0][i] = 0;
-        }
-
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(text1[i - 1] == text2[j - 1]){
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
-
-        // for(int i=0;i<=n;i++){
-        //     for(int j=0;j<=m;j++){
-        //         cout<<dp[i][j]<< " ";
-        //     }
-        //     cout<<endl;
-        // }
-
-        return dp[n][m];
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>>dp(n, vector<int>(m, -1));
+        return solve(0, 0, text1, text2, dp);
     }
 };
