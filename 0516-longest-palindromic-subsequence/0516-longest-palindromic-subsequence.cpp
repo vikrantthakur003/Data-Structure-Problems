812886220044bc048de4714980e9350c4b6a6dd6
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-08-07 19:07:24 +0530

    Time: 83 ms (51.02%), Space: 76 MB (21.52%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,46 +1,36 @@
 class Solution {
 public:
-    int solve(int ind1, int ind2, string &s1, string &s2, vector<vector<int>> &dp){
-        if(ind1 >= s1.size() || ind2 >= s2.size()){
+    int solve(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
+        if(i >= s1.size() || j >= s2.size()){
             return 0;
         }
-        if(dp[ind1][ind2] != -1){
-            return dp[ind1][ind2];
-        }
-        if(s1[ind1] == s2[ind2]){
-            return 1 + solve(ind1 + 1, ind2 + 1, s1, s2, dp);
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
 
-        return dp[ind1][ind2] = max(solve(ind1 + 1, ind2, s1, s2, dp), solve(ind1, ind2 + 1, s1, s2, dp));
+        int takeValue = 0;
+        int notTakeValue = 0;
+        if(s1[i] == s2[j]){
+            takeValue = 1 + solve(i + 1, j + 1, s1, s2, dp);
+        } else {
+            int skipFromFirst = solve(i + 1, j, s1, s2, dp);
+            int skipFromSecond = solve(i, j + 1, s1, s2, dp);
+
+            notTakeValue = max(skipFromFirst, skipFromSecond);
+        }
+
+        return dp[i][j] = max(takeValue, notTakeValue);
     }
     int longestPalindromeSubseq(string s) {
-        string revereString = s;
-        
-        reverse(revereString.begin(), revereString.end());
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+
         int n = s.size();
-        int m = revereString.size();
-        // vector<vector<int>> dp(n, vector<int>(m, -1));
-        // return solve(0, 0, s, revereString, dp);
+        int m = s2.size();
 
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        vector<vector<int>>dp(n, vector<int>(m, -1));
 
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 0;
-        }
-        for(int i=0;i<=m;i++){
-            dp[0][i] = 0;
-        }
-
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s[i - 1] == revereString[j - 1]){
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
-
-        return dp[n][m];
+        return solve(0, 0, s, s2, dp);
     }
 };
