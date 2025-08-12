e42493c299a4fc54c25bce1fa9fb1ba6f9565a7b
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-08-12 11:37:39 +0530

    Time: 4 ms (80.46%), Space: 14.2 MB (23.69%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,66 +1,34 @@
 class Solution {
 public:
-    int solve(int ind1, int ind2, string &s1, string& s2, vector<vector<int>> &dp) {
-        if (ind1 == s1.size()) {
-            return s2.size() - ind2;
-        }
-        if (ind2 == s2.size()) {
-            return s1.size() - ind1;
+    int solve(int i, int j, string &word1, string &word2, vector<vector<int>>&dp){
+        if(i == word1.size()){
+            return word2.size() - j;
         }
 
-        if(dp[ind1][ind2] != -1){
-            return dp[ind1][ind2];
+        if(j == word2.size()){
+            return word1.size() - i;
         }
 
-        if (s1[ind1] == s2[ind2]) {
-            return dp[ind1][ind2] = solve(ind1 + 1, ind2 + 1, s1, s2, dp);
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(word1[i] == word2[j]){
+            return dp[i][j] = solve(i + 1, j + 1, word1, word2, dp);
         } else {
-            // replacemenet
-            int replaceCharacter = 1 + solve(ind1 + 1, ind2 + 1, s1, s2, dp);
+            int replaceCharacter = 1 + solve(i + 1, j + 1, word1, word2, dp);
+            int removeCharacter = 1 + solve(i + 1, j, word1, word2, dp);
+            int insertCharacter = 1 + solve(i, j + 1, word1, word2, dp);
 
-            // removing
-            int removeCharacter = 1 + solve(ind1 + 1, ind2, s1, s2, dp);
-
-            // insertion
-            int insertCharacter = 1 + solve(ind1, ind2 + 1, s1, s2, dp);
-
-            return dp[ind1][ind2] = min(replaceCharacter, min(removeCharacter, insertCharacter));
+            return dp[i][j] = min({replaceCharacter, removeCharacter, insertCharacter});
         }
     }
     int minDistance(string word1, string word2) {
-
         if(word1 == word2){
             return 0;
         }
         int n = word1.size();
         int m = word2.size();
-        // vector<vector<int>> dp(n, vector<int>(m, -1));
-        // return solve(0, 0, word1, word2, dp);
-
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-
-        for(int i= 0; i<=n; i++){
-            dp[i][0] = i;
-        }
-
-        for(int i=0; i<=m; i++){
-            dp[0][i] = i;
-        }
-
-        for(int i=1;i<=n;i++){
-            for(int j=1; j<=m; j++){
-                if(word1[i - 1] == word2[j - 1]){
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else {
-                    int replaceCharacter = 1 + dp[i - 1][j - 1];
-                    int removeCharacter = 1 + dp[i - 1][j];
-                    int insertCharacter = 1 + dp[i][j - 1];
-
-                    dp[i][j] = min(replaceCharacter, min(removeCharacter, insertCharacter));
-                }
-            }
-        }
-
-        return dp[n][m];
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return solve(0, 0, word1, word2, dp);
     }
 };
