5d921b2b6db8015fe7071d631c1531b68f35d2e3
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-08-12 16:57:21 +0530

    Time: 11 ms (29.69%), Space: 13.2 MB (46.9%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -28,7 +28,33 @@ public:
         }
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(0, 0, word1, word2, dp);
+        // vector<vector<int>> dp(n, vector<int>(m, -1));
+        // return solve(0, 0, word1, word2, dp);
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for(int i = 0; i<=n; i++){
+            dp[i][0] = i;
+        }
+
+        for(int i = 0; i<=m; i++){
+            dp[0][i] = i;
+        }
+
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                if(word1[i - 1] == word2[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int replaceCharacter = 1 + dp[i - 1][j - 1];
+                    int removeCharacter = 1 + dp[i - 1][j];
+                    int insertCharacter = 1 + dp[i][j - 1];
+
+                    dp[i][j] = min({replaceCharacter, removeCharacter, insertCharacter});
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
