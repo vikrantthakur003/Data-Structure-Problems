52e55662b54117b60937a31824eeeb58cc71650f
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-08-13 18:24:43 +0530

    Time: 7 ms (21.74%), Space: 19.8 MB (24.17%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,31 +1,49 @@
 class Solution {
 public:
-    int solve(int i, bool canIbuy, vector<int> &price, vector<vector<int>> &dp){
-        if(i >= price.size()){
+    int solve(int ind, bool canIBuy, vector<int>& prices,
+              vector<vector<int>>& dp) {
+        if (ind >= prices.size()) {
             return 0;
         }
-        if(dp[i][canIbuy] != -1){
-            return dp[i][canIbuy];
+
+        if (dp[ind][canIBuy] != -1) {
+            return dp[ind][canIBuy];
         }
-        
+
         int profit = 0;
-        if(canIbuy){
-            int takeValue = -price[i] + solve(i + 1, !canIbuy, price, dp);
-            int notTakeValue = solve(i + 1, canIbuy, price, dp);
+        if (canIBuy) {
+            int buyValue = -prices[ind] + solve(ind + 1, !canIBuy, prices, dp);
+            int notBuyValue = solve(ind + 1, canIBuy, prices, dp);
 
-            profit = max(takeValue, notTakeValue);
+            profit = max(buyValue, notBuyValue);
         } else {
-            int takeValue = price[i] + solve(i + 1, !canIbuy, price, dp);
-            int notTakeValue = solve(i + 1, canIbuy, price, dp);
+            int sellValue = prices[ind] + solve(ind + 1, !canIBuy, prices, dp);
+            int notSellValue = solve(ind + 1, canIBuy, prices, dp);
 
-            profit = max(takeValue, notTakeValue);
+            profit = max(sellValue, notSellValue);
         }
 
-        return dp[i][canIbuy] = profit;
+        return dp[ind][canIBuy] = profit;
     }
     int maxProfit(vector<int>& prices) {
+        // vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
+        // return solve(0, true, prices, dp);
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return solve(0, true, prices, dp);
+        vector<vector<int>> dp(n, vector<int>(2, 0));
+
+        dp[0][1] = -prices[0];
+
+        for (int i = 1; i < n; i++) {
+            int buyValue = -prices[i] + dp[i - 1][0];
+            int notBuyValue = dp[i - 1][1];
+
+            dp[i][1] = max(buyValue, notBuyValue);
+            int sellValue = prices[i] + dp[i - 1][1];
+            int notSellValue = dp[i - 1][0];
+
+            dp[i][0] = max(sellValue, notSellValue);
+        }
+
+        return dp[n - 1][0];
     }
 };
