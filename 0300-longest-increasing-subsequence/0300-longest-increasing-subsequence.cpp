2c03300032caf7e30c35c3a70ed88bade218b61b
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-08-20 13:12:52 +0530

    Time: 563 ms (16.39%), Space: 295.2 MB (37.6%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,55 +1,25 @@
 class Solution {
 public:
-    int solve(int ind, int lastIndex, vector<int> & arr, vector<vector<int>> &dp){
-        if(ind >= arr.size()){
+    int solve(int index, int lastIndex, vector<int>&nums, vector<vector<int>>&dp){
+        if(index >= nums.size()){
             return 0;
         }
 
-        if(dp[ind][lastIndex + 1] != -1){
-            return dp[ind][lastIndex + 1];
+        if(dp[index][lastIndex + 1] != -1){
+            return dp[index][lastIndex + 1];
         }
 
         int takeValue = 0;
-        if(lastIndex == -1 || arr[ind] > arr[lastIndex]){
-            takeValue = 1 + solve(ind + 1, ind, arr, dp);
-        }
+        if(lastIndex == -1 || nums[index] > nums[lastIndex]){
+            takeValue = 1 + solve(index + 1, index, nums, dp);
+        } 
 
-        int notTakeValue = solve(ind + 1, lastIndex, arr, dp);
-
-        return dp[ind][lastIndex + 1] = max(takeValue, notTakeValue);
+        int notTakeValue = solve(index + 1, lastIndex, nums, dp);
+        return dp[index][lastIndex + 1] = max(takeValue, notTakeValue);
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        // vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        // return solve(0, -1, nums, dp);
-
-        // vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-        
-        // for(int i = n - 1; i >= 0; i--){
-        //     for(int lastIndex = i - 1; lastIndex >= -1; lastIndex--){
-        //         int takeValue = 0;
-        //         if(lastIndex == -1 || nums[i] > nums[lastIndex]){
-        //             takeValue = 1 + dp[i + 1][i + 1];
-        //         }
-        //         int notTakeValue = dp[i + 1][lastIndex + 1];
-
-        //         dp[i][lastIndex + 1] = max(takeValue, notTakeValue);
-        //     }
-        // }
-        // return dp[0][0];
-
-
-        vector<int> dp(n, 1);
-        for(int i=0;i<n;i++){
-            for(int lastIndex = 0; lastIndex < i; lastIndex++){
-                if(nums[lastIndex] < nums[i]){
-                    dp[i] = max(1 + dp[lastIndex], dp[i]);
-                }
-            }
-        }
-
-        int maximumValue = *max_element(dp.begin(), dp.end());
-        return maximumValue;
-
+        vector<vector<int>> dp(n, vector<int> (n, -1));
+        return solve(0, -1, nums, dp);
     }
 };
