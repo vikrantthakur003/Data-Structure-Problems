4e842323968ddbb3816fd18c07c80de187c70102
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-08-22 19:03:27 +0530

    Time: 34 ms (49.55%), Space: 14 MB (51.29%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -1,50 +1,29 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<int>& cuts, vector<vector<int>> &dp) {
-        if (i + 1 == j) {
+    int solve(int i, int j, vector<int> &arr, vector<vector<int>> &dp){
+        if(i + 1 == j){
             return 0;
         }
-
         if(dp[i][j] != -1){
             return dp[i][j];
         }
+
         int minValue = INT_MAX;
-        for (int k = i + 1; k < j; k++) {
-            int cost = (cuts[j] - cuts[i]) + solve(i, k, cuts, dp) +
-                       solve(k, j, cuts, dp);
-            minValue = min(minValue, cost);
+        for(int k = i + 1; k<j; k++){
+            int value = (arr[j] - arr[i]) + solve(i, k, arr, dp) + solve(k, j, arr, dp);
+
+            minValue = min(minValue, value);
         }
 
         return dp[i][j] = minValue;
     }
-    int minCost(int n, vector<int>& cuts) { 
+    int minCost(int n, vector<int>& cuts) {
         cuts.push_back(0);
         cuts.push_back(n);
 
         sort(cuts.begin(), cuts.end());
-        int c = cuts.size(); 
-        // vector<vector<int>> dp(c, vector<int>(c, -1));
-
-        // return solve(0, cuts.size() - 1, cuts, dp); 
-
-        vector<vector<int>> dp(c, vector<int>(c, 0));
-
-        for(int i = c - 1; i>= 0; i--){
-            for(int j = i + 1; j < c; j++){
-                int minValue = INT_MAX;
-                for(int k = i + 1; k<j; k++){
-                    int cost = (cuts[j] - cuts[i]) + dp[i][k] + dp[k][j];
-
-                    minValue = min(minValue, cost);
-                }
-                if(minValue == INT_MAX){
-                    dp[i][j] = 0;
-                } else {
-                    dp[i][j] = minValue;
-                }
-            }
-        }
-
-        return dp[0][c - 1];
+        int size = cuts.size();
+        vector<vector<int>> dp(size, vector<int> (size, -1));
+        return solve(0, cuts.size() - 1, cuts, dp);
     }
 };
