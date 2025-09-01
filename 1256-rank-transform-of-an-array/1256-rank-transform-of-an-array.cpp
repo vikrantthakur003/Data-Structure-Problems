0f87f28dd876ff4cccd26a7763b039d3eaaa693f
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-09-01 12:51:38 +0530

    Time: 56 ms (37.06%), Space: 46.3 MB (23.25%) - LeetHub

---
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        int count = 0; 
+        int n = arr.size();
+        vector<int> result;
+       
+        priority_queue<int, vector<int>, greater<int>> minheap;
+        for(int i = 0; i<n; i++){
+            minheap.push(arr[i]);
+        }
+
+        unordered_map<int, int> mp; // element, rank;
+        while(!minheap.empty()){
+            int topElement = minheap.top();
+            minheap.pop();
+
+            if(mp.find(topElement) == mp.end()){
+                count++;
+                mp[topElement] = count;
+            } else {
+                mp[topElement] = mp[topElement];
+            }
+        }
+
+        for(int i = 0; i<n; i++){
+            result.push_back(mp[arr[i]]);
+        }
+
+        return result;
+    }
+};
