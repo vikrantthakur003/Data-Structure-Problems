32f8fe19b5b3e078ad317e37373f635a05943635
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-11-03 23:56:56 +0530

    Time: 6 ms (6.4%), Space: 14.9 MB (6.31%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> result;
-    void findPerfectCombination3(int k, int target, vector<int> temp, vector<int> &arr, int index){
-        if(temp.size() == k && target == 0){
-             result.push_back(temp);
+    void findCombinations(int ind, int k, int sum, vector<int> temp){
+
+        if(sum == 0 && temp.size() == k){
+            result.push_back(temp);
             return;
         }
 
-        if(index >= arr.size()){
+        if(temp.size() > k){
+            return;
+        }
+
+        if(ind > 9){
             return;
         }
         
-        temp.push_back(arr[index]);
-        findPerfectCombination3(k, target - arr[index], temp, arr, index + 1);
+        temp.push_back(ind);
+        findCombinations(ind + 1, k, sum - ind, temp);
 
         temp.pop_back();
-
-        findPerfectCombination3(k , target, temp, arr, index + 1);
+        findCombinations(ind + 1, k, sum, temp);
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> temp;
-        vector<int> arr;
-        for(int i=1;i<=9;i++){
-            arr.push_back(i);
-        }
+        vector<int>temp;
 
-     findPerfectCombination3(k, n, temp, arr, 0);
+        findCombinations(1, k, n, temp);
 
         return result;
     }
