cee7ff66a54473b56d51488c73d33de55240b391
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-11-06 17:36:11 +0530

    Time: 403 ms (6.89%), Space: 28.4 MB (7.93%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // moore's voting algorithms
-        int ele;
-        int count = 0;
+        unordered_map<int, int> mp; // element, frequency
 
-        for(int i=0;i<nums.size();i++){
-            if(count == 0){
-                 count = 1;
-                ele = nums[i];
-            }else if(nums[i] == ele){
-                count++;
-            }else{
-                count--;
+        int n = nums.size();
+        pair<int, int> result = {-1, -1}; // element frequency
+
+        for(int i = 0; i<n; i++){
+            cout<<mp[nums[i]]<<endl;
+            if(mp.find(nums[i]) != mp.end() && mp[nums[i]] >= n/2){
+                cout<<(mp[nums[i]] > n/2)<<endl;
+               int freq = mp[nums[i]] > n/2;
+               if(result.second < freq){
+                    result.first = nums[i];
+                    result.second = freq;
+               }
             }
+
+            mp[nums[i]]++;
         }
 
-        int checkMajorityElement = 0;
-        for(int i=0;i<nums.size();i++){
-            if(ele == nums[i]){
-                checkMajorityElement++;
-            }
-        }
-        if(checkMajorityElement > (nums.size()/2)){
-            return ele;
-        }
-        return -1;
+        return result.first;
     }
 };
