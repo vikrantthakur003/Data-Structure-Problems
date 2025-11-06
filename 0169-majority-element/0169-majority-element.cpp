0c00af1a93ef3d38284f510c8138b8ed43e22225
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-11-06 18:43:22 +0530

    Time: 0 ms (100%), Space: 28.1 MB (87.12%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mp; // element, frequency
-
         int n = nums.size();
-        pair<int, int> result = {-1, -1}; // element frequency
+        int element = 0;
+        int count = 0;
 
         for(int i = 0; i<n; i++){
-            cout<<mp[nums[i]]<<endl;
-            if(mp.find(nums[i]) != mp.end() && mp[nums[i]] >= n/2){
-                cout<<(mp[nums[i]] > n/2)<<endl;
-               int freq = mp[nums[i]] > n/2;
-               if(result.second < freq){
-                    result.first = nums[i];
-                    result.second = freq;
-               }
+            if(count == 0){
+                element = nums[i];
+                count = 1;
+            } else if(element == nums[i]){
+                count++;
+            } else {
+                count--;
             }
-
-            mp[nums[i]]++;
         }
 
-        return result.first;
+        return element;
     }
 };
