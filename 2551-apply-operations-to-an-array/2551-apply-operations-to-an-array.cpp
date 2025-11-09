5006fa786e6444ed1049ec62be55f6f768284911
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-11-09 11:25:43 +0530

    Time: 0 ms (100%), Space: 13.2 MB (60.25%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        
+        int n = nums.size();
+        for(int i = 0; i<n-1; i++){
+            if(nums[i] == nums[i + 1]){
+                nums[i] = nums[i] * 2;
+                nums[i + 1] = 0;
+            }
+        }
+
+        int noOfZeros = 0;
+        int k = 0;
+        for(int i = 0; i<n; i++){
+            if(nums[i] == 0){
+                noOfZeros++;
+            } else {
+                nums[k++] = nums[i];
+            }
+        }
+
+        while(noOfZeros != 0){
+            nums[k++] = 0;
+            noOfZeros--;
+        }
+
+        return nums;
+    }
+};
