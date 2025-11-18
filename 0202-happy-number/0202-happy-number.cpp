d0f827d6ea896ad94971571d661ac0ed61c9e891
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-11-18 22:32:57 +0530

    Time: 0 ms (100%), Space: 7.6 MB (97.06%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
+    int squareOfNumber(int n){
+        int sum = 0;
+
+        while(n > 0){
+            int digit = n % 10;
+            sum += digit * digit;
+
+            n = n / 10;
+        }
+        return sum;
+    }
     bool isHappy(int n) {
-        
-        unordered_set<int> visited;
-
-        while(n != 1 && !visited.count(n)){
-            visited.insert(n);
-
-            int sum = 0;
-            while (n > 0){
-                int digit = n % 10;
-                sum +=  digit * digit;
-
-                n = n/10;
-            }
-            n = sum;
+        if(n == 1){
+            return true;
         }
 
-        return n == 1;
+        int slow = n;
+        int fast = squareOfNumber(n);
+
+        while(fast != 1 && slow != fast){
+            slow = squareOfNumber(slow);
+            fast = squareOfNumber(squareOfNumber(fast));
+        }
+
+        return fast == 1;
     }
 };
