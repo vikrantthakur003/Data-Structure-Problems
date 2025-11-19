9ba9fabc5075a93a3c6f838b167654fff6b32db3
Vikrant Thakur <91746016+vikrantthakur003@users.noreply.github.com>
2025-11-19 13:32:27 +0530

    Time: 0 ms (100%), Space: 13.7 MB (88.58%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        
-        string str = "";
-        int count = 1;
-        str += chars[0];
 
-        for(int i = 1; i<chars.size(); i++){
-            if(chars[i] != chars[i - 1]) {
-                if(count > 1){
-                    str += to_string(count);
+        int count = 1;
+        int k = 0;
+        chars[k++] = chars[0];
+
+        for (int i = 1; i < chars.size(); i++) {
+            if (chars[i] != chars[i - 1]) {
+                if (count > 1) {
+                    string cntString = to_string(count);
+                    for (int i = 0; i < cntString.length(); i++) {
+                        chars[k++] = cntString[i];
+                    }
                 }
                 count = 1;
-                str += chars[i];
+                chars[k++] = chars[i];
             } else {
                 count++;
             }
         }
 
-        if(count > 1){
-            str += to_string(count);
+        if (count > 1) {
+            string cntString = to_string(count);
+            for (int i = 0; i < cntString.length(); i++) {
+                chars[k++] = cntString[i];
+            }
         }
 
-        int k = 0;
-        for(int i = 0; i<str.length(); i++){
-            chars[k] = str[i];
-            k++;
-        }
-
-        return str.length();
+        return k;
     }
 };
